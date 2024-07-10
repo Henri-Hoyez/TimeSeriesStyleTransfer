095c7727f42044862878e75670d011150eee5cde
Henri-Hoyez <henri.hoyez@gmail.com>
2024-07-10 08:46:38 +0200

    Feat: Stabilization of the architecture on Simulated Data

---
+++ configs/args.py
@@ -0,0 +1,5 @@
+class AmplitudeShiftArgs():
+    content_dataset_path = "data/simulated_dataset/01 - Source Domain.h5"
+    style1_dataset_path = "data/simulated_dataset/amplitude_shift/1.0_1.0.h5"
+    style2_dataset_path = "data/simulated_dataset/amplitude_shift/4.5_4.5.h5"
+
